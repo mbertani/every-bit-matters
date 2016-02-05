4bc2ea1e6beed394d828a7bee7ea919884c157fe
Marco Bertani-Økland <mbertani@gmail.com>
2016-02-05 14:04:42 +0100

    excersice 0 finished

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /*
     TODO: Create a web server to serve content for the client.
 */
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static('client'));
+
+app.listen(port, function () {
+    console.log('Server running on http://localhost:' + port + '/');
+});
